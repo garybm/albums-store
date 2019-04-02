c0a5988302daceaff828e8d2e5589a4e863c1888
Gary Bermudez and Kimberly McConnell <gary@>
2019-04-02 11:09:06 -0700

    add data file and model for album data

---
+++ src/app/album.model.ts
@@ -0,0 +1,8 @@
+export class Album {
+  constructor (
+    public title: string,
+    public atrist: string,
+    public description: string,
+    public id: number
+  ) { }
+}
--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from '../album.model';
+import { albumData } from '../album-data';
 
 @Component({
   selector: 'app-marketplace',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./marketplace.component.scss']
 })
 export class MarketplaceComponent implements OnInit {
-
+  albums: Album[] = albumData;
   constructor() { }
 
   ngOnInit() {
+    console.log("albums: ", this.albums);
   }
 
 }
