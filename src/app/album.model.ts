591c3d16d9ffd48e934370baa7478882d1432d09
Gary Bermudez and Riley Benson <gary@>
2019-04-03 11:10:29 -0700

    save data in a new firebase database

--- src/app/album.model.ts
+++ src/app/album.model.ts
@@ -3,7 +3,6 @@ export class Album {
   constructor (
     public title: string,
     public artist: string,
-    public description: string,
-    public id: number
+    public description: string
   ) { }
 }
